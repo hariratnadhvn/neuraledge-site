48e0b1e0f1d638ebe6c02ef246a604e2beb9c504
Rathna Kumar <hariratnadhvn@hotmail.com>
2025-07-07 23:50:05 +0530

    Update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/neuraledge-site/', // 👈 This is required for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
